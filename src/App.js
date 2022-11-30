39971fbe94c2c687e7465c5c64682280e615404c
LEEDAESEUNG-Developer <eotmd1016@gmail.com>
2022-11-30 20:01:32 +0900

    <22/11/30 20:02> 백업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+    getStyle = {
+        border: "1px solid black",
+        marginBottom: "10px"
+    }
+
+    handleSubmit = (event) =>{
+        //form 주소 이동 방지
+        event.preventDefault();
+
+        let value = this.state.value;
+
+        // 객세 생성
+        let newObject = {
+            id: Date.now(),
+            message: value
+        }
+
+        this.setState({objects: [...this.state.objects, newObject], value: ""});
+        console.log("status ", this.state);
+    };
+
+    keyChange = (event) =>{
+        let eventKey = event.target.value;
+        this.setState({value: eventKey});
+    }
+
+    delete = (id) => {
+        let filterObjects = this.state.objects.filter(object => object.id !== id);
+        this.setState({objects: filterObjects});
+    }
+
+    state = {
+        objects :[
+            {
+                id: "1",
+                message: "내용",
+            },
+            {
+                id: "2",
+                message: "내용2",
+            },
+        ],
+        value:""
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <div className="items">
+                    {this.state.objects.map(item => (
+                        <div style={this.getStyle} key={item.id}>
+                            ID: {item.id} <button type="button" onClick={() => this.delete(item.id)}>삭제</button> <br/>
+                            내용: {item.message}
+                        </div>
+                    ))}
+                </div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="메시지를 입력하세요" value={this.state.value} onChange={this.keyChange}/>
+                    <button type="submit">저장</button>
+                </form>
+            </div>
+        );
+    }
+}
